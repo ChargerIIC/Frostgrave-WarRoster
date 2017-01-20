181f72327cf44b9979594207b57cb7976cb508d9
ChargerIIC <ChargerIIC@Yahoo.com>
2017-01-20 13:20:49 -0800

    Addition of Casting Level
    Target number in order to succeed at casting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,9 @@ export class AppComponent {
     this.wizard = new Mage();
     var spell = new Spell();
     spell.name = "Crumble";
-    spell.description = "Crumble Description";
+    spell.description = "This spell only works against man-made structures such as buildings and walls. The spellcaster rapidly speeds up the passing of time in a small area of the structure, causing it to collapse. This can effectively create a doorway-sized hole through any wall, which should be indicated on the table somehow. The spell can also be used to collapse a section of floor beneath a figure standing on a level above the ground. In this case, the figure about to be affected must pass a Will roll versus the casting roll, or fall to the next level down and taking damage appropriately.";
     spell.school = "Chronomancer";
+    spell.castingNumber = 10;
     this.wizard.spells[0] = spell;
   }
 }
--- src/app/model/spell.model.ts
+++ src/app/model/spell.model.ts
@@ -3,4 +3,5 @@ export class Spell{
   name:string;
   description:string;
   school: string;
+  castingNumber: number;
 }
