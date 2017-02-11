dcd92ca55d781be73f2af498815f622e4f29848a
Vernon Burt <ChargerIIC@yahoo.com>
2017-02-10 19:58:10 -0800

    Thug added to wargame vault
    Thug added to wargame vault

--- src/app/model/warbandVault.model.ts
+++ src/app/model/warbandVault.model.ts
@@ -1,3 +1,4 @@
+import { EquipmentVault } from './equipmentVault.model';
 import { Minion } from './minion.model';
 
 ///This is a repo class for the various minions that can be hired for a warband.
@@ -17,11 +18,21 @@ export class WarbandVault {
     minion.shoot = 0;
     minion.armor = 10;
     minion.will = 2;
-    minion.health = 8
+    minion.health = 8;
     minion.cost = 10;
     minion.notes = "Animal, Cannot carry items"
     this.templates.push(minion);
 
-    
+    minion = new Minion('Thug');
+    minion.move = 6;
+    minion.fight = 2;
+    minion.shoot = 0;
+    minion.armor = 10;
+    minion.will = -1;
+    minion.health = 10;
+    minion.cost = 20;
+    minion.notes = ""
+    minion.heldItem = EquipmentVault.getItemByName('Hand Weapon');
+    this.templates.push(minion);
     }
 }
