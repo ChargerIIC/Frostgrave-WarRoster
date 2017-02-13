4ce07b7c146b8877a5084ae90bb64942d7c66d42
ChargerIIC <ChargerIIC@Yahoo.com>
2017-02-13 12:16:57 -0800

    Equipment from warband members added
    Bow, Crossbow, Staff, Two-handed Weapon, Healing Potion

--- src/app/model/equipmentVault.model.ts
+++ src/app/model/equipmentVault.model.ts
@@ -15,7 +15,7 @@ export class EquipmentVault{
     item.name = "Staff";
     item.bonusType = "Fight";
     item.bonusNum = 0;
-    item.description = "A default fighting weapon for a wizard. No bonus for equipping";
+    item.description = "A default fighting weapon for a wizard. Reduces enemy attacks by 1 damage.";
     item.cost = 0;
     EquipmentVault.items.push(item);
 
@@ -33,9 +33,46 @@ export class EquipmentVault{
     item.name = "Dagger";
     item.bonusType = "Fight";
     item.bonusNum = 1;
-    item.description = "A default fighting weapon held in an alterante hand. Stacks with Hand Weapon";
+    item.description = "A default fighting weapon held in an alterante hand. -1 damage modifier. Stacks with Hand Weapon";
     item.cost = 0;
     EquipmentVault.items.push(item);
+
+    item = new Equipment();
+    item.id = "W04";
+    item.name = "Bow";
+    item.bonusType = "Fight";
+    item.bonusNum = 0;
+    item.description = "24 inch range.";
+    item.cost = 0;
+    EquipmentVault.items.push(item);
+
+    item = new Equipment();
+    item.id = "W05";
+    item.name = "Crossbow";
+    item.bonusType = "Fight";
+    item.bonusNum = 0;
+    item.description = "24 inch range. +2 damage. Requires an action to reload.";
+    item.cost = 0;
+    EquipmentVault.items.push(item);
+
+    item = new Equipment();
+    item.id = "W06";
+    item.name = "Two-Handed Weapon";
+    item.bonusType = "Fight";
+    item.bonusNum = 0;
+    item.description = "+2 damage.";
+    item.cost = 0;
+    EquipmentVault.items.push(item);
+
+    item = new Equipment();
+    item.id = "P07";
+    item.name = "Healing Potion";
+    item.bonusType = "Fight";
+    item.bonusNum = 0;
+    item.description = "Restores up to 5 lost health on use";
+    item.cost = 50;
+    EquipmentVault.items.push(item);
+
   }
 
   static getItemByName(name: string):Equipment{
