2390f6a2eca5270d04812624c70e0d87ad09dba2
ChargerIIC <ChargerIIC@Yahoo.com>
2017-02-16 17:38:14 -0800

    Home base types from core populated
    Going to need to decide if we will only support core resources or if
    expansion data should be added.

--- src/app/model/homebaseVault.model.ts
+++ src/app/model/homebaseVault.model.ts
@@ -26,5 +26,26 @@ export class HomeBaseVault{
     base.name = 'Temple';
     base.description = 'Once a place of worship, these ruins project an aura of calm. +1 bonus to casting rolls for Miraculous Cure and Restore Life spells';
     this.baseOptions.push(base);
+
+    base = new HomeBase();
+    base.name = 'Crypt';
+    base.description = 'While creepy, it does contain plenty of necromancy supplies. +2 to Raise Zombie spells';
+    this.baseOptions.push(base);
+
+    base = new HomeBase();
+    base.name = 'Treasury';
+    base.description = 'This treasury has several sealed vaults that have not been opened yet. At the end of each game, the warband may attempt to open one of the vaults. Roll 1d20. On a result of 2-18 you may add that many gold pieces to your inventory. On a 19 add 100 gold pieces. On a 20 a treasure is found and should be treated the same as a treasure found in game. On a 1 than a single warband member of the players choice is injured and cannot particpate in the next game.';
+    this.baseOptions.push(base);
+
+    base = new HomeBase();
+    base.name = 'Brewery';
+    base.description = 'The promise of well aged alcohol left in the brewery cheapens your recuritment costs. New warband members cost 5 gold pieces less and an extra 10 gold pieces is earned after each game.';
+    this.baseOptions.push(base);
+
+    base = new HomeBase();
+    base.name = 'Library';
+    base.description = 'A few useful tomes have surivied in this old library. Reading them gives the wizard an extra 20 experience points after each game.';
+    this.baseOptions.push(base);
+
   }
 }
