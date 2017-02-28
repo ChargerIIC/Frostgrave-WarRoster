4376251790d2a59c9134048ea88a4545c77215aa
ChargerIIC <ChargerIIC@Yahoo.com>
2017-02-28 14:06:49 -0800

    Level Up Options added
    Can add to a stat, can add a spell slot, can improve ability to cast

--- src/app/levelup-dialog-modal/levelup-dialog-modal.component.ts
+++ src/app/levelup-dialog-modal/levelup-dialog-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Mage } from '../model/mage.model';
+import { Spell } from '../model/spell.model';
 
 @Component({
   selector: 'app-levelup-dialog-modal',
@@ -16,10 +17,45 @@ export class LevelupDialogModalComponent implements OnInit {
   }
 
   addLevel(){
+    this.userMage.level++;
     this.userMage.levelUpPoints++;
   }
 
   showOptions(){
     return this.userMage.levelUpPoints > 0;
+  }
+
+  addFight(){
+    this.userMage.fight++;
+    this.userMage.levelUpPoints--;
+  }
+
+  addShoot(){
+    this.userMage.shoot++;
+    this.userMage.levelUpPoints--;
+  }
+
+  addWill(){
+    this.userMage.will++;
+    this.userMage.levelUpPoints--;
+  }
+
+  addHealth(){
+    this.userMage.health++;
+    this.userMage.levelUpPoints--;
+  }
+
+  spellsPresent(){
+    return this.userMage.spells.length > 0;
+  }
+
+  improveSpell(spell : Spell){
+    var index = this.userMage.spells.indexOf(spell);
+    this.userMage.spells[index].castingNumber--;
+    this.userMage.levelUpPoints--;
+  }
+
+  addSpellSlot(){
+    this.userMage.spellSlotsAvailable['Any']++;
   }
 }
