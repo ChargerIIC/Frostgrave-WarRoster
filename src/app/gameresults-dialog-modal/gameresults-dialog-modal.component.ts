39ad67a03d294ed9261aa8088ac2427697af9e68
ChargerIIC <ChargerIIC@Yahoo.com>
2017-03-03 14:54:22 -0800

    Basic ability to add casualties
    Not real causalty tracking, but all the hooks are there.

--- src/app/gameresults-dialog-modal/gameresults-dialog-modal.component.ts
+++ src/app/gameresults-dialog-modal/gameresults-dialog-modal.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Mage } from '../model/mage.model';
+import { Minion } from '../model/minion.model';
 
 @Component({
   selector: 'app-gameresults-dialog-modal',
   templateUrl: './gameresults-dialog-modal.component.html',
   styleUrls: ['./gameresults-dialog-modal.component.css']
 })
+
 export class GameresultsDialogModalComponent implements OnInit {
 
   @Input() userMage: Mage;
 
   experience: number;
+  selectedWarbandMember: Minion;
 
   constructor() { }
 
@@ -20,4 +23,15 @@ export class GameresultsDialogModalComponent implements OnInit {
   applyResults(){
     this.userMage.experience += this.experience;
   }
+
+  onCasualtySelected(newValue){
+    this.selectedWarbandMember = newValue;
+  }
+
+  addCasaulty(){
+    var index = this.userMage.warbandMembers.indexOf(this.selectedWarbandMember);
+    var minion = this.userMage.warbandMembers[index];
+    minion.notes += ' WOUNDED';
+  }
+
 }
