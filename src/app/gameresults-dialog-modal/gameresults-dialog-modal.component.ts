74bb8478e60c126125b37c0e962525b71ff0d756
ChargerIIC <ChargerIIC@Yahoo.com>
2017-03-06 14:41:04 -0800

    Wounded Status updated for random dice roll per the book
    Wounded Status updated for random dice roll per the book

--- src/app/gameresults-dialog-modal/gameresults-dialog-modal.component.ts
+++ src/app/gameresults-dialog-modal/gameresults-dialog-modal.component.ts
@@ -14,6 +14,7 @@ export class GameresultsDialogModalComponent implements OnInit {
 
   experience: number;
   selectedWarbandMember: Minion;
+  actionLog: string;
 
   constructor() { }
 
@@ -31,7 +32,24 @@ export class GameresultsDialogModalComponent implements OnInit {
   addCasaulty(){
     var index = this.userMage.warbandMembers.indexOf(this.selectedWarbandMember);
     var minion = this.userMage.warbandMembers[index];
-    minion.status = 'WOUNDED';
+
+    //roll 1d20
+    //TODO: Might need to create the classic Dice Roller Utility
+    var roll = Math.floor(Math.random() * 20) + 1;
+    this.actionLog = "Rolling for casualty results: " + roll + '. ';
+    if(roll <= 4){
+      this.userMage.warbandMembers.splice(index, 1); //Died
+      this.actionLog = minion.name += ' was killed by thier wounds.';
+    }
+    else if(roll <=8){
+      minion.status = 'WOUNDED';
+      this.actionLog = minion.name += ' was wounded.'
+    }
+    else{
+      this.actionLog = minion.name += ' survived thier injury.';
+    }
+
+    
   }
 
 }
